124490093182d0bacd901f1cb9f57ecfd5b0fcab
xuanxuanhaha <zhangqixuan17@outlook.com>
2024-10-03 22:23:44 +1000

    change word

--- psychologyLaravel/resources/js/components/DataGenerate/DataGenerate.js
+++ psychologyLaravel/resources/js/components/DataGenerate/DataGenerate.js
@@ -60,7 +60,7 @@ const DataGenerate = () => {
 
     data.map((answer) => {
           if(answer.useremail) {
-            const answerToString = `"${answer.userid}","${answer.groupcode === 1 ? 'BASC' : (answer.groupcode === 2 ? 'BASC' : 'C')}","${answer.useremail ? answer.useremail.replace(/"/g, '""') : '-'}","${answer.sessionid}","${answer.questionno ? answer.questionno.replace(/"/g, '""') : '-'}","${answer.response ? answer.response.replace(/"/g, '""') : '-'}","${answer.firstopenat ? TimestampToDateTimeExcel(answer.firstopenat) : '-'}","${answer.endat ? TimestampToDateTimeExcel(answer.endat) : '-'}"`;
+            const answerToString = `"${answer.userid}","${answer.groupcode === 1 ? 'BASCSI' : (answer.groupcode === 2 ? 'BASC' : 'C')}","${answer.useremail ? answer.useremail.replace(/"/g, '""') : '-'}","${answer.sessionid}","${answer.questionno ? answer.questionno.replace(/"/g, '""') : '-'}","${answer.response ? answer.response.replace(/"/g, '""') : '-'}","${answer.firstopenat ? TimestampToDateTimeExcel(answer.firstopenat) : '-'}","${answer.endat ? TimestampToDateTimeExcel(answer.endat) : '-'}"`;
             csvContent = `${csvContent}\n${answerToString}`;
           }
       });
